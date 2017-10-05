64db13fce8e3c6d6c4a768a36a705dfbfd604c7b
Alexander Shabanov <avshabanov@gmail.com>
2017-10-04 17:31:08 -0700

    Add IR1

---
+++ scv-experiments/src/main/java/com/truward/scv/experiments/synth/IR1.java
@@ -0,0 +1,17 @@
+package com.truward.scv.experiments.synth;
+
+/**
+ * TBD
+ */
+public class IR1 {
+
+  // represents parameterized result of a function call
+  interface Result<T> {
+  }
+
+  interface Methods {
+
+    //Result<>
+  }
+
+}
